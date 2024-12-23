463e173dfb7840ac3e34ad70d7dcfa87ec48f105
ayoung19 <andyluyoung@gmail.com>
2024-12-23 03:57:34 -0800

    fix: add pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,41 @@
+import { IconButton, Stack, Text } from "@chakra-ui/react";
+import { usePagination } from "@mantine/hooks";
+import { FiChevronLeft, FiChevronRight, FiMoreHorizontal } from "react-icons/fi";
+
+interface Props {
+  pagination: ReturnType<typeof usePagination>;
+}
+
+export const Pagination = ({ pagination }: Props) => {
+  return (
+    <Stack direction="row">
+      <IconButton
+        aria-label="Paginate previous"
+        icon={<FiChevronLeft />}
+        isDisabled={pagination.active === 1}
+        onClick={() => pagination.previous()}
+      />
+      {pagination.range.map((n, i) =>
+        n === "dots" ? (
+          <Stack key={i} alignItems="center" justifyContent="center" w="8" h="8">
+            <FiMoreHorizontal />
+          </Stack>
+        ) : (
+          <IconButton
+            key={i}
+            colorScheme={pagination.active === n ? "purple" : undefined}
+            aria-label={`Paginate page ${n}`}
+            icon={<Text fontSize="xs">{n}</Text>}
+            onClick={() => pagination.setPage(n)}
+          />
+        ),
+      )}
+      <IconButton
+        aria-label="Paginate next"
+        icon={<FiChevronRight />}
+        isDisabled={pagination.active === pagination.range.length}
+        onClick={() => pagination.next()}
+      />
+    </Stack>
+  );
+};
