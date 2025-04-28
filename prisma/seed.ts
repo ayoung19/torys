1719d39dd24930c95c829e94e03b4e49f2ffc570
ayoung19 <andyluyoung@gmail.com>
2025-04-28 10:04:27 -0700

    chore: seed productiond data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,18 @@
-import { AccountType, PrismaClient } from "@prisma/client";
+import { AccountType, JobType, PrismaClient } from "@prisma/client";
+import employeesJSON from "./employees.json";
+import jobsJSON from "./jobs.json";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.entry.deleteMany();
+  await prisma.day.deleteMany();
+  await prisma.employee.deleteMany();
+  await prisma.job.deleteMany();
+  await prisma.timesheet.deleteMany();
+  await prisma.action.deleteMany();
+  await prisma.account.deleteMany();
+
   if (process.env.DEV_ACCOUNT_ID) {
     const devAccount = await prisma.account.upsert({
       where: { accountId: process.env.DEV_ACCOUNT_ID },
@@ -17,6 +27,50 @@ async function main() {
 
     console.log(devAccount);
   }
+
+  await prisma.timesheet.create({
+    data: {
+      timesheetId: "2025-05-03",
+    },
+  });
+
+  await prisma.employee.createMany({
+    data: employeesJSON.map((employee) => ({
+      timesheetId: "2025-05-03",
+      ...employee,
+      fringeCode: "",
+    })),
+  });
+
+  await prisma.job.createMany({
+    data: jobsJSON.map((job) => ({
+      timesheetId: "2025-05-03",
+      ...job,
+      jobType:
+        job.jobType === "STATE"
+          ? JobType.STATE
+          : job.jobType === "FEDERAL"
+            ? JobType.FEDERAL
+            : JobType.PRIVATE,
+    })),
+  });
+
+  const jobs = await prisma.job.findMany({
+    where: {
+      timesheetId: "2025-05-03",
+    },
+  });
+
+  await prisma.day.createMany({
+    data: jobs.flatMap((job) =>
+      Array.from(Array(7).keys()).map((dayId) => ({
+        timesheetId: "2025-05-03",
+        jobId: job.jobId,
+        dayId,
+        description: "",
+      })),
+    ),
+  });
 }
 
 main()
