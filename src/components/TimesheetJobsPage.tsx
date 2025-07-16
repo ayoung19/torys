6d96dabcb68ef08a6ba767afa85a58ec5eb8189a
ayoung19 <andyluyoung@gmail.com>
2025-07-16 12:50:11 -0700

    chore: hide inactive jobs

--- src/components/TimesheetJobsPage.tsx
+++ src/components/TimesheetJobsPage.tsx
@@ -35,6 +35,7 @@ export const TimesheetJobsPage = ({ timesheet, jobs }: Props) => {
       </Breadcrumb>
       {jobs
         .toSorted((a, b) => a.name.localeCompare(b.name))
+        .filter((job) => job.isActive)
         .map((job) => (
           <Card key={job.jobId}>
             <CardBody>
